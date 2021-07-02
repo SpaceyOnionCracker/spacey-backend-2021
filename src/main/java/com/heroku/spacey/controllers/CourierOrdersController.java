b90b877d6ec63c65d0f34ac9e7161a3f2c5b304c
Khrystyna <khrystynarak@gmail.com>
2021-07-02 15:55:05 +0300

    Add security to courier orders

--- src/main/java/com/heroku/spacey/controllers/CourierOrdersController.java
+++ src/main/java/com/heroku/spacey/controllers/CourierOrdersController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import com.heroku.spacey.dto.order.CourierOrdersDto;
 import com.heroku.spacey.services.CourierOrdersService;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.Date;
@@ -11,12 +12,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/profile/orders")
+@RequestMapping("/api/v1/profile")
 public class CourierOrdersController {
 
     private final CourierOrdersService courierOrdersService;
 
-    @GetMapping("")
+    @Secured("ROLE_COURIER")
+    @GetMapping("/orders")
     public List<CourierOrdersDto> getOrders(
             @RequestParam(required = false) Date date) {
         return courierOrdersService.getCourierOrders(date);
