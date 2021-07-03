22889ed232f68d752696a374cabb5a96fa8efa5a
Katatyunyaa <katerynahaletska@gmail.com>
2021-07-03 15:43:17 +0300

    Add userId with token in service

--- src/main/java/com/heroku/spacey/services/impl/OrderDetailsServiceImpl.java
+++ src/main/java/com/heroku/spacey/services/impl/OrderDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import com.heroku.spacey.dao.OrderDetailsDao;
 import com.heroku.spacey.dto.order.OrderDetailsDto;
 import com.heroku.spacey.dto.order.OrderStatusDto;
 import com.heroku.spacey.services.OrderDetailsService;
+import com.heroku.spacey.utils.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +16,11 @@ import java.sql.SQLException;
 public class OrderDetailsServiceImpl implements OrderDetailsService {
 
     private final OrderDetailsDao orderDetailsDao;
+    private final SecurityUtils securityUtils;
 
     @Override
     public OrderDetailsDto getOrderDetails(Long orderId) throws SQLException {
-        return orderDetailsDao.getOrderDetails(orderId);
+        return orderDetailsDao.getOrderDetails(orderId, securityUtils.getUserIdByToken());
     }
 
     @Override
